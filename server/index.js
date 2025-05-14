5b03f9c7839a3f8fd4bea457a432cd654ff146ec
Git-Faz <fahad.blr@icloud.com>
2025-05-14 20:59:49 +0530

    server changes

--- server/index.js
+++ server/index.js
@@ -44,7 +44,7 @@ app.use(cors({
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin','expires'],
   exposedHeaders: ['Set-Cookie', 'Date', 'ETag']
 }));
 
